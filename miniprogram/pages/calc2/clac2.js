2748cf9a63b72ac34c308d0efe6939ac2f8a69e5
unknown <2112913396@qq.com>
2025-02-15 00:31:49 +0800

    buchong3

--- miniprogram/pages/calc2/clac2.js
+++ miniprogram/pages/calc2/clac2.js
@@ -1,5 +1,6 @@
 // pages/calc2/clac2.js
 Page({
+  data:{
   result:"0",
   id1:"history",
   id2:"clear",
@@ -21,76 +22,14 @@ Page({
   id18:"num_0",
   id19:"dot",
   id20:"equ",
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
   },
   clickbutton:function(e){
     var data = this.data.result;
     if(e.target.id>='num_0'&&e.target.id<='num_9'){
-      if(this.result=='0'){
-      data=e.target.id.split("")[1];}
+      if(this.data.result=='0'){
+      data=e.target.id.split("_")[1];}
       else{
-      data+=e.target.id.split("")[1];}
+      data+=e.target.id.split("_")[1];}
     }
     else{
       console.log(e.target.id);
